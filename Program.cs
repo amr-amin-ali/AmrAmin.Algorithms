0b37da3eea3c0608e2bc9781799aa7d286afba55
Amro Amin <a.amin@ps-tt.com>
2024-07-24 10:05:45 +0300

    Bubble Sort Algorithm.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
-﻿public class Program
+﻿using AmrAmin.Algorithms.SortingAlgorithms.BubbleSort;
+
+public class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int[] arr = [1, 9, 5, 6, 3, 7, 4, 2, 8];
+        BubbleSort.Sort(arr);
+
     }
 }
---
+++ SortingAlgorithms/BubbleSort/BubbleSort.cs
@@ -0,0 +1,48 @@
+﻿namespace AmrAmin.Algorithms.SortingAlgorithms.BubbleSort;
+public class BubbleSort
+{
+    public static void Sort(int[] arr)
+    {
+        Console.WriteLine(@"/==============================================================\");
+        PrintArray(arr: arr, title: "Bubble Sort Algorithm", message: "Array to sort:");
+        int n = arr.Length;
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                    // Swap arr[j] and arr[j+1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        PrintArray(arr: arr, message: "Sorted array");
+        Console.WriteLine(@"\==============================================================/");
+    }
+
+    private static void PrintArray(int[] arr, string message, string title = null)
+    {
+        if (title is not null)
+        {
+            Console.WriteLine($"|____________________{title}_____________________|");
+        }
+        Console.WriteLine($"|\t-{message}");
+        Console.Write("|\t\t[");
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (i == arr.Length - 1)
+            {
+                Console.Write($"{arr[i]}");
+
+            }
+            else
+            {
+                Console.Write($"{arr[i]}, ");
+            }
+        }
+        Console.Write("]\n");
+    }
+}
