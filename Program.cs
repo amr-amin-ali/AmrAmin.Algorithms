f685cff3b2955e4e8d8e02f91aa9cdcce44fecf7
Amro Amin <a.amin@ps-tt.com>
2024-07-24 14:11:03 +0300

    Refactor

--- Program.cs
+++ Program.cs
@@ -5,10 +5,13 @@ public class Program
 {
     public static void Main(string[] args)
     {
+        /////////////////////////////////////////
         int[] arr1 = [1, 9, 5, 6, 3, 7, 4, 2, 8];
         BubbleSort.Sort(arr1);
+        /////////////////////////////////////////
         int[] arr2 = [1, 9, 5, 6, 3, 7, 4, 2, 8];
         SelectionSort.Sort(arr2);
+        /////////////////////////////////////////
 
     }
 }
